8871531de20d3f3d733f34d5b3754c0e00d2c778
Jose Garcia <GT22004@ues.edu.sv>
2023-10-21 23:33:50 -0600

    Header&Footer

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full flex flex-row justify-between p-6 bg-blue-500 text-white'>
+    <div className='w-full flex flex-row justify-between p-6 bg-gt22004-primary-color text-white'>
       <Link href={'/'} className='text-xl font-bold'>React example</Link>
       <div className='flex flex-row gap-4'>
         <Link href={'/'}>Inicio</Link>
         <Link href={'/examples'}>Ejemplos</Link>
+        <Link href={'/usuarios'}>Usuarios</Link>
+        <Link href={'/login'}>Login</Link>
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'gt22004-primary-color': '#008000'
+      }
     },
   },
   plugins: [],
